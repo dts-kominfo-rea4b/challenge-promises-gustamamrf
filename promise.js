808ab77f8db436c4cb85aad7818346a65e0f1b39
gustamamrf <gustamamarufimelda@gmail.com>
2022-09-18 14:04:52 +0700

    membuat fungsi pada promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,13 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js');
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let THEATER_IXX = await promiseTheaterIXX();
+  let THEATER_VGC = await promiseTheaterVGC();
+
+  const jumlahEmosi = THEATER_IXX.filter((emosiTheaterIXX) => emosiTheaterIXX.hasil === emosi).length + THEATER_VGC.filter((emosiTheaterVGC) => emosiTheaterVGC.hasil === emosi).length;
+  return jumlahEmosi;
+};
 
 module.exports = {
   promiseOutput,
